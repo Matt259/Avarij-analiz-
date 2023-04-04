6540223496e8b4b2feea2dd665ce992f6cdce445
Matt259 <60190520+Matt259@users.noreply.github.com>
2023-04-04 19:28:45 +0300

    Add files via upload
    https://data.gov.lt/dataset/eismo-ivykiu-ivykusiu-lietuvos-respublikoje-kuriu-metu-zuvo-ir-ar-buvo-suzeisti-zmones-duomenys download json files that are meant to be read from here. Name them in this convention "accidents_{year}"

---
+++ main.py
@@ -0,0 +1,20 @@
+import prompt
+if __name__ == '__main__':
+    prompt.user_prompt()
+    
+        
+        # csv_writer = CSVWriter(drivers, FILE_NAMES[index]+".csv")
+        # csv_writer.write_to_csv()
+
+ 
+    
+
+    
+    
+    
+    
+        
+
+
+    
+
